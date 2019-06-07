d7bb7e827d297f39fd30fecfd532fdaebc496350
JustAClueless <51457226+JustAClueless@users.noreply.github.com>
2019-06-07 13:27:34 +0700

    Add files via upload

---
+++ SeaBattle/SeaBattleTest/PlayerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sea_Battle;
+
+namespace SeaBattleTest
+{
+    [TestClass]
+    public class PlayerTest
+    {
+        [TestMethod]
+        public void TestFindPoints()
+        {
+            var testPalyer = new Player();
+            int[] result = null;
+            int[] actual = testPalyer.findPoints();
+            Assert.AreEqual(result, actual);
+        }
+    }
+}
---
+++ SeaBattle/SeaBattleTest/ShipTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sea_Battle;
+
+namespace SeaBattleTest
+{
+    [TestClass]
+    public class ShipTest
+    {
+        [TestMethod]
+        public void TestStateKilled()
+        {
+            int cellsNumber = 4,
+                number = 2;
+            var testShip = new Ship(cellsNumber, number);
+            State result = State.killed;
+            State actual = testShip.state();
+            Assert.AreEqual(result, actual);
+        }
+
+        [TestMethod]
+        public void TestStateHurt()
+        {
+            int cellsNumber = 4,
+                number = 3;
+            var testShip = new Ship(cellsNumber, number);
+            State result = State.hurt;
+            State actual = testShip.state();
+            Assert.AreEqual(result, actual);
+        }
+
+        [TestMethod]
+        public void TestState()
+        {
+            int cellsNumber = 4,
+                number = 2;
+            var testShip = new Ship(cellsNumber, number);
+            State result = (State)number;
+            State actual = testShip.state();
+            Assert.AreEqual(result, actual);
+        }
+    }
+}
